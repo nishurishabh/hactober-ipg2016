2ee5de9efb28015cfc3f0d0d4ce5fa552bf29faa
nishurishabh <39476553+nishurishabh@users.noreply.github.com>
2019-10-01 23:54:10 +0530

    Update bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -22,3 +22,51 @@ int bfs(int s) {
 
     return visCount;
 }
+
+// also print primes 
+// C++ program to find the prime numbers 
+// between a given interval 
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+int main() 
+{ 
+	int a, b, i, j, flag; 
+	cout << "Enter lower bound of the interval: "; 
+	cin >> a; 
+
+	
+	cout << "\nEnter upper bound of the interval: "; 
+	cin >> b;
+
+	
+	cout << "\nPrime numbers between "
+		<< a << " and " << b << " are: "; 
+
+	
+	for (i = a; i <= b; i++) 
+	{ 
+		
+		if (i == 1 || i == 0) 
+			continue; 
+
+		
+		flag = 1; 
+
+		for (j = 2; j <= i / 2; ++j) 
+		{ 
+			if (i % j == 0) 
+			{ 
+				flag = 0; 
+				break; 
+			} 
+		} 
+
+		if (flag == 1) 
+			cout << i << " "<<endl; 
+	} 
+
+	return 0; 
+} 
+
